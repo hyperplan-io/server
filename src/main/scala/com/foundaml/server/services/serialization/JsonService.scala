b326b581995a1f2fdb73bdfe9fe2065af9437a2b
Antoine Sauray <sauray.antoine@gmail.com>
2019-03-07 09:01:10 +0100

    add circe serialization

---
+++ src/main/scala/com/foundaml/server/services/serialization/JsonService.scala
@@ -0,0 +1,12 @@
+package com.foundaml.server.services.serialization
+
+import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+import scalaz.zio. { IO } 
+case class JsonError(message: String) extends Throwable {
+	
+}
+
+class JsonService {
+
+  def toJson[Data](data: Data)(implicit encoder: io.circe.Encoder[Data]) = data.asJson.noSpaces
+}
