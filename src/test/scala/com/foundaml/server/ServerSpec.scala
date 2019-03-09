e30eefba2b9ecf46042a6522ad8629b8cf512248
Antoine Sauray <sauray.antoine@gmail.com>
2019-03-09 12:15:01 +0100

    renamed test

--- src/test/scala/com/foundaml/server/ServerSpec.scala
+++ src/test/scala/com/foundaml/server/ServerSpec.scala
@@ -25,5 +25,7 @@ class HelloWorldSpec extends org.specs2.mutable.Specification {
     retHelloWorld.status must beEqualTo(Status.Ok)
 
   private[this] def uriReturnsHelloWorld(): MatchResult[String] =
-    retHelloWorld.as[String].unsafeRunSync() must beEqualTo("{\"message\":\"Hello, world\"}")
+    retHelloWorld.as[String].unsafeRunSync() must beEqualTo(
+      "{\"message\":\"Hello, world\"}"
+    )
 }
