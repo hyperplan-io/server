9c62e8c4067e59777cd6489879d6bb3dc147ce0a
Antoine Sauray <sauray.antoine@gmail.com>
2019-03-10 13:29:56 +0100

    add more constraints on sql

--- conf/default.sql
+++ conf/default.sql
@@ -1,15 +1,17 @@
 
 CREATE TABLE IF NOT EXISTS projects(
   id VARCHAR(36) PRIMARY KEY,
-  name TEXT,
-  problem TEXT,
-  default_algorithm VARCHAR(36),
+  name TEXT NOT NULL,
+  problem TEXT NOT NULL,
+  algorithm_policy VARCHAR(36) NOT NULL,
+  feature_class TEXT NOT NULL,
+  label_class TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS algorithms(
   id VARCHAR(36) PRIMARY KEY,
-  backend TEXT,
-  project_id VARCHAR(36) REFERENCES projects(id)
+  backend TEXT NOT NULL ,
+  project_id VARCHAR(36) REFERENCES projects(id) NOT NULL 
 );
 
 
