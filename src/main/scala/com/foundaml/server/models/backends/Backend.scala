e44362bee6217ce76835e5e0a93dab47b84370b0
Antoine Sauray <sauray.antoine@gmail.com>
2019-03-14 08:13:26 +0100

    clean imports in Backend.scala

--- src/main/scala/com/foundaml/server/models/backends/Backend.scala
+++ src/main/scala/com/foundaml/server/models/backends/Backend.scala
@@ -1,15 +1,7 @@
 package com.foundaml.server.models.backends
 
-import com.foundaml.server.models.features._
 import com.foundaml.server.models.features.transformers._
 import com.foundaml.server.models.labels._
-
-import io.circe.generic.extras.auto._
-import io.circe.generic.extras.Configuration
-import io.circe.generic.JsonCodec
-import io.circe.parser.decode, io.circe.syntax._
-import io.circe.generic.extras.Configuration, io.circe.generic.extras.auto._
-
 
 sealed trait Backend
 
