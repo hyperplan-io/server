b5851a5f99211b9ca90f8162604145ecdba98612
Antoine Sauray <antoinesauray@users.noreply.github.com>
2019-03-19 08:52:58 +0100

    update kinesis config and publish in examples stream (#39)
    * update kinesis config and publish in examples stream
    
    * fix test kinesis config

--- src/main/scala/com/foundaml/server/domain/FoundaMLConfig.scala
+++ src/main/scala/com/foundaml/server/domain/FoundaMLConfig.scala
@@ -1,5 +1,9 @@
 package com.foundaml.server.domain
 
-case class KinesisConfig(enabled: Boolean, predictionsStream: String)
+case class KinesisConfig(
+    enabled: Boolean,
+    predictionsStream: String,
+    examplesStream: String
+)
 
 case class FoundaMLConfig(kinesis: KinesisConfig)
