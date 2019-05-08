b0a70a5b00a753acf1c878ad79b61f27cd9dffb0
Antoine Sauray <antoinesauray@users.noreply.github.com>
2019-05-08 12:42:03 +0200

    Create domain service (#77)
    * add controller for domain GET and POST
    
    * add logs when a project and algorithm or an example is created
    
    * gst

---
+++ app/src/main/scala/com/foundaml/server/domain/services/DomainService.scala
@@ -0,0 +1,11 @@
+package com.foundaml.server.domain.services
+
+import cats.effect.IO
+import cats.implicits._
+import com.foundaml.server.infrastructure.logging.IOLogging
+
+class DomainService() extends IOLogging {
+
+  def readDomain = ???
+  def createDomainModel = ???
+}
