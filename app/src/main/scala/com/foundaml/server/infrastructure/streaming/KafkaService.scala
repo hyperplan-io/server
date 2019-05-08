71c8de165ac3d8b9625887eb6385b358e4e8e77e
Antoine Sauray <sauray.antoine@gmail.com>
2019-05-08 16:11:23 +0200

    create kafka service

---
+++ app/src/main/scala/com/foundaml/server/infrastructure/streaming/KafkaService.scala
@@ -0,0 +1,31 @@
+package com.foundaml.server.infrastructure.streaming
+
+import com.foundaml.server.infrastructure.logging.IOLogging
+import cats.effect.IO
+import io.circe.Encoder
+
+import fs2.kafka._
+
+class KafkaService(topic: String, stream: fs2.Stream[IO, KafkaProducer[IO,String,String]]) extends IOLogging {
+
+  def publish[Data](
+    data: Data,
+    partitionKey: String
+  )(implicit circeEncoder: Encoder[Data]) = {
+    val json = circeEncoder(data).noSpaces
+    val record = ProducerRecord(topic, partitionKey, json)
+    ProducerMessage.one(record)
+  }
+
+}
+
+object KafkaService {
+  import org.apache.kafka.common.serialization.StringSerializer
+  import cats.effect.ContextShift
+  def apply(topic: String, bootstrapServers: String)(implicit cs: ContextShift[IO]): IO[KafkaService] = for {
+    producerSettings <- IO(
+      ProducerSettings[String, String].withBootstrapServers(bootstrapServers)
+    )
+    producer = fs2.kafka.producerStream[IO].using(producerSettings)
+  } yield new KafkaService(topic, producer)
+}
