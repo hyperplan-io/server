be3a11f491d2424ac2702a8286da6bb611c13f65
Antoine Sauray <sauray.antoine@gmail.com>
2019-07-21 11:13:02 +0200

    handle bad features dimensions

--- app/src/main/scala/com/hyperplan/domain/errors/FeaturesError.scala
+++ app/src/main/scala/com/hyperplan/domain/errors/FeaturesError.scala
@@ -1,11 +1,14 @@
 package com.hyperplan.domain.errors
+import com.hyperplan.domain.models.features.FeatureDimension
 
 sealed trait FeaturesError
 
 case class FeaturesDoesNotExistError(featuresId: String) extends FeaturesError
 case class FeaturesAlreadyExistError(featuresId: String) extends FeaturesError
 case class ReferenceFeatureDoesNotExistError() extends FeaturesError
-case class UnsupportedDimensionError(featureClass: String, dimension: String)
-    extends FeaturesError
+case class UnsupportedDimensionError(
+    featureClass: String,
+    dimension: FeatureDimension
+) extends FeaturesError
 case class RecursiveFeatureError(featureClass: String, dimension: String)
     extends FeaturesError
